bc86b48de9b2a0ef86c19f3ccd469a71466c26ff
alex <alexanderstapp@gmail.com>
2021-05-18 00:54:56 -0600

    finalized testing

--- src/__tests__/utilFunctions.test.js
+++ src/__tests__/utilFunctions.test.js
@@ -11,3 +11,18 @@ test('shortenText should limit to 100 char and add elipses to the end', () => {
     expect(shortened).not.toHaveLength(longText.length)
     expect(shortened.slice(-3)).toBe('...')
 })
+
+test('wordCount should return word count', () => {
+    expect(wordCount(posts)).toBe(233);
+});
+  
+test('attachUserName should correctly attach a users full name to a post', () => {
+    const newPosts = attachUserName(users, posts);
+    expect(newPosts[0]).toHaveProperty('displayName');
+});
+  
+test('attachUserName should remove any post with no matching user', () => {
+    const newPosts = attachUserName(users, posts);
+    const deletedPost = posts[5];
+    expect(newPosts).not.toContainEqual(deletedPost);
+});
